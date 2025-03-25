9ea5dc718b9de2438eeee3d161803b6eb1b9a3ec
Sai tarun Sirapurapu <tarunsirapurapiu@gmail.com>
2025-03-25 19:33:29 -0400

    Changed project layout

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,25 +6,22 @@ import nc from "../assets/newscurator.jpg";
 function Projects() {
   return (
     <div className="projects-container">
-      <h2>Projects</h2>
+      <h2>Personal Projects</h2>
       <div className="projects-grid">
-        <div className="project-card">
-          <img src={reaImage} alt="Regression" className="project-image" />
+        <div className="project-card vertical">
+          <a href="http://ec2-18-116-72-123.us-east-2.compute.amazonaws.com/" target="_blank" rel="noopener noreferrer">
+            <img src={reaImage} alt="Regression" className="project-image" />
+          </a>
           <div className="project-details">
             <h2>Real Estate Analysis</h2>
-            <p>Regression model for real-estate price prediction.
-              Developed using python on backend and react on front end.
-              Deployed on AWS EC2 service.
+            <p>
+              Regression model for real-estate price prediction. Developed using Python on backend and React on front end. Deployed on AWS EC2.
             </p>
             <p><strong>Tech Used:</strong> Python, pandas, Flask, React</p>
-            <div className="button-group">
-              <a href="http://ec2-18-116-72-123.us-east-2.compute.amazonaws.com/" className="btn outline">Demo</a>
-              <a href="https://github.com/TARUNGIT98/RealEstateAnalysis" className="btn filled">Source Code</a>
-            </div>
           </div>
         </div>
 
-        <div className="project-card">
+        {/* <div className="project-card">
           <img src="https://via.placeholder.com/150" alt="Chatbot AI" className="project-image" />
           <div className="project-details">
             <h2>Chatbot AI</h2>
@@ -35,42 +32,35 @@ function Projects() {
               <a href="#" className="btn filled">Source Code</a>
             </div>
           </div>
-        </div>
+        </div> */}
 
-        <div className="project-card">
-          <img src={mega} alt="Climate App" className="project-image" />
+        <div className="project-card vertical">
+          <a href="https://tarungit98.github.io/megasandesham/" target="_blank" rel="noopener noreferrer">
+            <img src={mega} alt="Climate App" className="project-image" />
+          </a>
+
           <div className="project-details">
             <h2>Megasandesham!</h2>
             <p>
-              This dynamic, responsive weather app is built with React and Tailwind CSS, and it integrates the OpenWeatherMap API to
-              deliver real-time weather data. The application features condition-based weather cards that dynamically adjust their
+              This dynamic, responsive weather application features condition-based weather cards that dynamically adjust their
               backgrounds and icons to reflect current conditions, ensuring an engaging visual experience.
               Smooth transitions and modern design elements create a polished, consumer-friendly experience.
             </p>
-            <p><strong>Tech Used:</strong> React, Tailwind CSS</p>
-            <div className="button-group">
-              <a href="https://tarungit98.github.io/megasandesham/" className="btn outline">Demo</a>
-              <a href="https://github.com/TARUNGIT98/megasandesham" className="btn filled">Source Code</a>
-            </div>
+            <p><strong>Tech Used:</strong> React, Tailwind CSS, OpenweatherMap API</p>
           </div>
         </div>
 
         <div className="project-card">
-          <img src={nc} alt="Climate App" className="project-image" />
+          <a href="https://ainewscurator.netlify.app/" target="_blank" rel="noopener noreferrer">
+            <img src={nc} alt="Climate App" className="project-image" />
+          </a>
           <div className="project-details">
             <h2>AI News Curator</h2>
             <p>
               Personalized News Curator is a full-stack application that curates real-time news based on user preferences and selected regions.
-              The backend fetches top headlines via NewsAPI and uses an AI filtering service (leveraging DJL,Hugging Face and semantic embeddings) to rank and refine articles.
-              Users can customize news categories and regions, ensuring a tailored news feed.
               Still working on refining the articles when it comes to differnt regions and ultimate goal is to have a simple prompt to pull all necessary articles.!
             </p>
             <p><strong>Tech Used:</strong> Java, DJL, React, Docker, Postgres, Render</p>
-            <div className="button-group">
-              <a href="https://ainewscurator.netlify.app/" className="btn outline">Demo</a>
-              <a href="https://github.com/TARUNGIT98/newsAggregator-backend" className="btn filled">Back End</a>
-              <a href="https://github.com/TARUNGIT98/newsAggregator-frontend" className="btn filled">Front End</a>
-            </div>
           </div>
         </div>
 
