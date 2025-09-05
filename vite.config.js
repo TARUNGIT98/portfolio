54e8ae12fb98f9cffb84bd6c8b7e3b632d1bb5be
Sai tarun Sirapurapu <tarunsirapurapiu@gmail.com>
2025-09-04 21:13:07 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/", // ✅ MUST be root since you're I'm on a custom domain
-// });
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Correct for custom domain
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-  },
+  base: "/", // ✅ MUST be root since you're I'm on a custom domain
 });
